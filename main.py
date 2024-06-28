3268a08ff491deb5cefc5edc16046ad5ddda4e5b
Snehal Patil <snehalpatil2011@gmail.com>
2024-06-29 02:04:40 +0530

    Add Post call for order placer

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ async def health_check():
     print("Webhook Triggered")
     return "The health check is Successfull !"
 
+@app.post("/placeorder")
+async def place_order():
+    print("Webhook Triggered: Order Placer")
+    return "Order Placer called"
 
 if __name__ == "__main__":
     uvicorn.run(app)
