28f587bde549a11b30782bc8bec9ffe3e719c055
Snehal Patil <snehalpatil2011@gmail.com>
2024-06-29 13:29:20 +0530

    Post with Payload

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 import uvicorn
 
 app =  FastAPI()
 
+class Message(BaseModel):
+    message:str
 
 @app.get("/")
 async def health_check():
     print("Webhook Triggered")
     return "The health check is Successfull !"
 
-@app.post("/placeorder")
-async def place_order(data):
-    print("Webhook Triggered: Order Placer")
-    return "Order Placer called"
+@app.post("/placeorder/")
+async def place_order(message:Message):
+    print(f"Webhook Triggered: Order Placer | {message.message}")
+    return message
 
 if __name__ == "__main__":
     uvicorn.run(app)
